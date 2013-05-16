070a3ef670761ae2c4b508ee5ed70c152ecb4ed1
Rogowski Piotr <piotrekrogowski@gmail.com>
2013-05-16 11:45:05 +0200

    Hierarchia wyjątków

---
+++ src/net/komunikator/server/exceptions/ConnectionException.java
@@ -0,0 +1,7 @@
+package net.komunikator.server.exceptions;
+
+/**
+ * @author Piotr Rogowski<piotrekrogowski@gmail.com>
+ */
+public class ConnectionException extends ComunicatorException {
+}
---
+++ src/net/komunikator/server/exceptions/NotConnectedException.java
@@ -0,0 +1,7 @@
+package net.komunikator.server.exceptions;
+
+/**
+ * @author Piotr Rogowski<piotrekrogowski@gmail.com>
+ */
+public class NotConnectedException extends ConnectionException {
+}
