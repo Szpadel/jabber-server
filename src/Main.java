6e6d3a5faa64127c9327289879d4fbc49f363c22
Reut <michalreut0@gmail.com>
2013-06-09 02:50:42 +0200

    Wyjebałem śmieci (chuj wi co) z Main.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,7 @@
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.jdbc.support.SQLErrorCodes;
-
 public class Main {
 
-    @Qualifier("H2")
-    @Autowired
-    private SQLErrorCodes h2;
-
-
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
     }
 
 
